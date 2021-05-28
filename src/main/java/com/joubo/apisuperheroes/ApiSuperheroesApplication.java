b4628684b12154dfd6516ad535c78cef9df5da69
Jordi Bordoy <jordibordoy@gmail.com>
2021-05-28 10:55:20 +0200

    Creación del proyecto SpringBoot

---
+++ src/main/java/com/joubo/apisuperheroes/ApiSuperheroesApplication.java
@@ -0,0 +1,13 @@
+package com.joubo.apisuperheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSuperheroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSuperheroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joubo/apisuperheroes/ApiSuperheroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joubo.apisuperheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSuperheroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
